db4b30ceaba68fb6f3028f922b13158827320e08
Dmitry Vdovin <voidxnull@gmail.com>
2015-10-01 06:56:42 +0200

    Add incomplete bootstrap theme

---
+++ themes/bootstrap.js
@@ -0,0 +1,22 @@
+export default {
+  Button: {
+    button: 'btn btn-default',
+    active: 'active',
+    disabled: 'disabled'
+  },
+  CheckButtonGroup: {
+    buttonGroup: 'btn-group'
+  },
+  RadioButtonGroup: {
+    buttonGroup: 'btn-group'
+  },
+  Tabs: {
+    tabs: '',
+    menu: 'nav nav-tabs',
+    menuItem: '',
+    menuItemLink: '',
+    menuItemLinkActive: '',
+    menuItemActive: 'active',
+    tab: 'tab-content'
+  }
+};
