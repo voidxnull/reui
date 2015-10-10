4421b3e527fd941d8e5149a63f4413830ca8c968
Dmitry Vdovin <voidxnull@gmail.com>
2015-10-10 09:45:05 +0200

    Fix Panel typos

--- lib/components/Panel.jsx
+++ lib/components/Panel.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import BaseComponet from './BaseComponent';
+import BaseComponent from './BaseComponent';
 import themeable from 'react-themeable';
 
 export default class Panel extends BaseComponent {
@@ -9,7 +9,7 @@ export default class Panel extends BaseComponent {
     const theme = themeable(this._mixTheme());
 
     return (
-      <div {theme(1, 'panel')}>
+      <div {...theme(1, 'panel')}>
         {this.props.children}
       </div>
     );
