77e5e7da3f1a40f4f9e5e5be8e90c7e4ee217b89
Dmitry Vdovin <voidxnull@gmail.com>
2015-10-13 08:32:41 +0200

    Update example

--- example/allComponents.jsx
+++ example/allComponents.jsx
@@ -10,29 +10,39 @@ ReUI.setGlobalTheme(theme);
 
 let page = (
   <div>
-    <ReUI.Button title="Button" />
-    <ReUI.Button title="Active Button" active />
-    <ReUI.Button title="Disabled Button" disabled />
-    <ReUI.Tabs>
-      <ReUI.Tab title="One">
-        1
-      </ReUI.Tab>
-      <ReUI.Tab title="Two">
-        2
-      </ReUI.Tab>
-    </ReUI.Tabs>
-    <ReUI.RadioButtonGroup>
-      <ReUI.Button title="One" />
-      <ReUI.Button title="Two" />
-      <ReUI.Button title="Three" />
-      <ReUI.Button title="Four" />
-    </ReUI.RadioButtonGroup>
-    <ReUI.CheckButtonGroup>
-      <ReUI.Button title="One" />
-      <ReUI.Button title="Two" />
-      <ReUI.Button title="Three" />
-      <ReUI.Button title="Four" />
-    </ReUI.CheckButtonGroup>
+    <ReUI.Panel>
+      <ReUI.Button title="Button" />
+      <br />
+      <ReUI.Button title="Active Button" active />
+      <br />
+      <ReUI.Button title="Disabled Button" disabled />
+    </ReUI.Panel>
+    <ReUI.Panel>
+      <ReUI.Tabs>
+        <ReUI.Tab title="One">
+          1
+        </ReUI.Tab>
+        <ReUI.Tab title="Two">
+          2
+        </ReUI.Tab>
+      </ReUI.Tabs>
+    </ReUI.Panel>
+    <ReUI.Panel>
+      <ReUI.RadioButtonGroup>
+        <ReUI.Button title="One" />
+        <ReUI.Button title="Two" />
+        <ReUI.Button title="Three" />
+        <ReUI.Button title="Four" />
+      </ReUI.RadioButtonGroup>
+    </ReUI.Panel>
+    <ReUI.Panel>
+      <ReUI.CheckButtonGroup>
+        <ReUI.Button title="One" />
+        <ReUI.Button title="Two" />
+        <ReUI.Button title="Three" />
+        <ReUI.Button title="Four" />
+      </ReUI.CheckButtonGroup>
+    </ReUI.Panel>
   </div>
 );
 
