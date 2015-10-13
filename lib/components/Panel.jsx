6ae88f9b316d499a34fa3743df1406fb0b138124
Dmitry Vdovin <voidxnull@gmail.com>
2015-10-13 09:27:57 +0200

    Add functionality to Panel

--- lib/components/Panel.jsx
+++ lib/components/Panel.jsx
@@ -3,12 +3,27 @@ import BaseComponent from './BaseComponent';
 import themeable from 'react-themeable';
 
 export default class Panel extends BaseComponent {
+  static defaultProps = {
+    title: ''
+  };
+
+  static defaultTheme = {
+    panel: 'reui-panel',
+    panelHeading: 'reui-panel__heading',
+    panelBody: 'reui-panel__body'
+  };
+
   render() {
     const theme = themeable(this._mixTheme());
-
+    const heading = (this.props.title.length)
+                    ? <div {...theme(2, 'panelHeading')}>{this.props.title}</div>
+                    : null;
     return (
       <div {...theme(1, 'panel')}>
-        {this.props.children}
+        {heading}
+        <div {...theme(3, 'panelBody')}>
+          {this.props.children}
+        </div>
       </div>
     );
   }
