4c1cdeeea5b7077e0039fdfec1048700276ce70f
Dmitry Vdovin <voidxnull@gmail.com>
2015-10-26 15:58:34 +0200

    Refactor Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { getTheme } from '../utils';
 
 export default class Header extends React.Component {
   static propTypes = {
-    level: React.PropTypes.oneOf([1, 2, 3, 4, 5, 6]).isRequired
+    level: React.PropTypes.oneOf([1, 2, 3, 4, 5, 6])
   };
 
   static defaultProps = {
@@ -24,12 +24,12 @@ export default class Header extends React.Component {
     const theme = getTheme(this);
 
     switch (this.props.level) {
-      case 1: return <h1 {...this.props} {...theme(1, 'header', 'header1')}>{this.props.children}</h1>; break;
-      case 2: return <h2 {...this.props} {...theme(1, 'header', 'header2')}>{this.props.children}</h2>; break;
-      case 3: return <h3 {...this.props} {...theme(1, 'header', 'header3')}>{this.props.children}</h3>; break;
-      case 4: return <h4 {...this.props} {...theme(1, 'header', 'header4')}>{this.props.children}</h4>; break;
-      case 5: return <h5 {...this.props} {...theme(1, 'header', 'header5')}>{this.props.children}</h5>; break;
-      case 6: return <h6 {...this.props} {...theme(1, 'header', 'header6')}>{this.props.children}</h6>; break;
+      case 1: return <h1 {...this.props} {...theme(1, 'header', 'header1')} />; break;
+      case 2: return <h2 {...this.props} {...theme(1, 'header', 'header2')} />; break;
+      case 3: return <h3 {...this.props} {...theme(1, 'header', 'header3')} />; break;
+      case 4: return <h4 {...this.props} {...theme(1, 'header', 'header4')} />; break;
+      case 5: return <h5 {...this.props} {...theme(1, 'header', 'header5')} />; break;
+      case 6: return <h6 {...this.props} {...theme(1, 'header', 'header6')} />; break;
       default: return null;
     }
   }
